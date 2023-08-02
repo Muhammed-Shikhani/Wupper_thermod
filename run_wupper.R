e84234dbdcc43a09c1623b04c5c2f587af658e2a
robertladwig <ladwigjena@gmail.com>
2023-08-02 13:47:33 -0500

    calibration

--- run_wupper.R
+++ run_wupper.R
@@ -54,7 +54,6 @@ ggplot(result) +
 wq_parameters <- append(parameters, c(0.001 / 1000, 
                                       2, 1500 * 1e4, 100)) # Fnep, Fsed, Ased, diffred
 
-
 # initial water temperatures and do concentrations
 yini <- c(3,3, 10 * 1000/1e6  * wq_parameters[1], 10 * 1000/1e6  * wq_parameters[2]) 
 
@@ -64,13 +63,29 @@ if (file.exists('output.txt')){
 
 ice_on = TRUE # ice "simulation" on or off?
 
+# calibrate parameters
+wq_parameters[19] = 2.5
+
+boundary_cal = boundary %>%
+  mutate(Jsw = 1.3 * Jsw,
+         Tair = 1.3 * Tair)
+
 # run the oxygen model
-out <- run_oxygen_model(bc = boundary, params = wq_parameters, ini = yini, times = times, ice = ice_on)
+out <- run_oxygen_model(bc = boundary_cal, params = wq_parameters, ini = yini, times = times, ice = ice_on)
 
 # visualize the results
 result <- data.frame('Time' = out[,1],
                      'WT_epi' = out[,2], 'WT_hyp' = out[,3],
                      'DO_epi' = out[,4], 'DO_hyp' = out[,5])
+
+ggplot(result) +
+  geom_line(aes(x=Time, y=WT_epi , col='Surface Mixed Layer')) +
+  geom_line(aes(x=(Time), y=WT_hyp , col='Bottom Layer')) +
+  labs(x = 'Simulated Time', y = 'WT in deg C')  +
+  theme_bw()+
+  guides(col=guide_legend(title="Layer")) +
+  theme(legend.position="bottom")
+
 ggplot(result) +
   geom_line(aes(x=Time, y=DO_epi / 1000 /  wq_parameters[1] * 1e6, col='Surface Mixed Layer')) +
   geom_line(aes(x=(Time), y=DO_hyp / 1000 /  wq_parameters[2] * 1e6, col='Bottom Layer')) +
